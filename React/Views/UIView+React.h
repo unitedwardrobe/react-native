d48fa551ff770ab75841644f80bfa490667b1b6d
Koen Punt <koen@koenpunt.nl>
2018-08-21 11:09:25 +0200

    update import for yoga
    when included as framework, cocoapods uses the `module_name` property of the podspec, but otherwise it uses the podspecs name

--- React/Views/UIView+React.h
+++ React/Views/UIView+React.h
@@ -8,7 +8,11 @@
 #import <UIKit/UIKit.h>
 
 #import <React/RCTComponent.h>
+#if __has_include(<ReactYoga/YGEnums.h>)
+#import <ReactYoga/YGEnums.h>
+#else
 #import <yoga/YGEnums.h>
+#endif
 
 @class RCTShadowView;
 
